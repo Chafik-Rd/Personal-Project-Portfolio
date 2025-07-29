8a8b23cc839956331663de7f7154c96ff8214def
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-30 02:00:04 +0700

    fix(App): edit style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Home } from "./components/Home"
 import { Navbar } from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-    <Navbar/>
-    </>
+    <div className="min-h-screen flex flex-col bg-linear-to-br from-mutedBlue-300 to-mutedBlue-200">
+      <Navbar/>
+      <Home/>
+    </div>
   )
 }
 
