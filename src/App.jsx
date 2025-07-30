5200545116b344a6cad1fe1778728bca6f16c1bf
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-31 00:28:24 +0700

    fix(App): add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { AboutMe } from "./components/AboutMe"
 import { Home } from "./components/Home"
 import { Navbar } from "./components/Navbar"
+import { Project } from "./components/Projects"
+import { Skills } from "./components/Skills"
 
 function App() {
 
   return (
-    <div className="min-h-screen flex flex-col bg-linear-to-br from-mutedBlue-300 to-mutedBlue-200">
+    <div className="min-h-screen bg-linear-to-br from-mutedBlue-300 to-mutedBlue-200">
       <Navbar/>
       <Home/>
+      <AboutMe/>
+      <Skills/>
+      <Project/>
     </div>
   )
 }
