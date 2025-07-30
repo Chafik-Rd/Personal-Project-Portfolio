877e757eec545ed4c2c749e24a6879e1044dc9f2
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-30 19:35:18 +0700

    fix(PageHeader): edit style responsive

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,10 +2,10 @@ export const PageHeader = ({header,text}) => {
     return (
         <div className="max-w-200 flex flex-col gap-6">
             <div className="flex flex-col gap-2 items-center">
-                <h2 className="text-4xl font-bold capitalize">{header}</h2>
+                <h2 className="text-3xl md:text-4xl font-bold capitalize">{header}</h2>
                 <span className="w-25 h-1 bg-linear-to-r from-mutedBlue-300 to-turquoise-400"></span>
             </div>
-            <p className="text-mutedBlueGray text-2xl font-medium text-center">{text}</p>
+            <p className="text-mutedBlueGray text-base md:text-2xl font-medium text-center">{text}</p>
         </div>
     )
 }
