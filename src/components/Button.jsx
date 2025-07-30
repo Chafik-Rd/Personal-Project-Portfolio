f644436a7259ec3b597287ea373952d81487cd88
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-31 00:27:09 +0700

    fix(Button): edit style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,14 @@ export const Button = ({
   text,
   style,
   icon = {
-    url: "./src/assets/images/down-arrow.svg",
+    url: "./src/assets/images/icons/down-arrow.svg",
     alt: "icon down-arrow",
   },
   iconStyle = "w-6",
 }) => {
   return (
     <button
-      className={`px-8 py-4 rounded-xl flex gap-2 justify-center shadow-md cursor-pointer ${style}`}
+      className={`rounded-xl flex gap-2 justify-center items-center shadow-md cursor-pointer hover:scale-95 ${style}`}
     >
       <img src={icon.url} alt={icon.alt} className={iconStyle} />
       {text}
