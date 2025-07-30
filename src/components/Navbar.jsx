3603973c4efb85f50fae901336c486bf93e04f11
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-31 00:26:11 +0700

    fix(Navbar): edit color style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 export const Navbar = () => {
     return (
-        <nav className="sticky top-0 z-10">
+        <nav className="sticky top-0 z-10 bg-white/80 backdrop-blur-md">
             <div className="flex justify-between items-center px-12 py-4">
                 <section>
                     <p className="text-3xl text-transparent bg-clip-text bg-linear-to-br from-mutedBlue-300 to-turquoise-400 font-bold">CR</p>
                 </section>
                 <section>
-                    <img src="./src/assets/images/menu.svg" className="md:hidden"/>
+                    <img src="./src/assets/images/icons/menu.svg" className="md:hidden"/>
                     <ul className="hidden md:flex gap-6">
                         <li className="cursor-pointer hover:scale-95">Home</li>
                         <li className="cursor-pointer hover:scale-95">About me</li>
