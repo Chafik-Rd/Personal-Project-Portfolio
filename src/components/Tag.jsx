fc28689ef927d8d1f3745cdd969a76c9adde1f92
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-07-31 00:24:37 +0700

    feat(Tag): created tag component

---
+++ src/components/Tag.jsx
@@ -0,0 +1,7 @@
+export const Tag = ({text}) => {
+    return (
+        <div className="px-3 py-1 rounded-xl bg-turquoise-100 text-mutedBlue-300 text-sm font-medium w-fit">
+            <p>{text}</p>
+        </div>
+    )
+}
