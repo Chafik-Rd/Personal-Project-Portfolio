ce95fffdfde76dcfb90cf5bb5ff88cc827643642
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-04 21:51:58 +0700

    fix(Navbar): change style when scroll page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react"
 export const Navbar = () => {
+    const [scrollY, setScrollY] = useState(0);
+    useEffect(() => {
+      const handleScroll = () => {
+        setScrollY(window.scrollY);
+      }
+      window.addEventListener("scroll", handleScroll);
+    },[scrollY]);
     return (
-        <nav className="sticky top-0 z-10 bg-white/80 backdrop-blur-md">
+        <nav className={`sticky top-0 z-10 backdrop-blur-md bg-mutedBlue-300 ${scrollY>100?"bg-white/80":""}`}>
             <div className="flex justify-between items-center px-12 py-4">
                 <section>
                     <p className="text-3xl text-transparent bg-clip-text bg-linear-to-br from-mutedBlue-300 to-turquoise-400 font-bold">CR</p>
@@ -10,7 +18,7 @@ export const Navbar = () => {
                     <ul className="hidden md:flex gap-6">
                         <li className="cursor-pointer hover:scale-95">Home</li>
                         <li className="cursor-pointer hover:scale-95">About me</li>
-                        <li className="cursor-pointer hover:scale-95">Skills</li>
+                        <li className="cursor-pointer hover:scale-95">Tech Stack</li>
                         <li className="cursor-pointer hover:scale-95">Projects</li>
                         <li className="cursor-pointer hover:scale-95">Experience</li>
                         <li className="cursor-pointer hover:scale-95">Contact</li>
