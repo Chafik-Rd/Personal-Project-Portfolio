2276c42957336200ba3d513e03ea8653a98f432f
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-04 22:06:08 +0700

    fix(Skills): change style skill card and add animation scroll

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,7 @@
+export const SkillCard = ({icon}) => {
+  return (
+    <li className="flex justify-center items-center h-20 w-45  mx-4 md:mx-8">
+      <img src={icon.url} alt={icon.alt} className="h-15 w-15 object-cover" />
+    </li>
+  );
+};
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,124 +1,32 @@
 import { PageHeader } from "./PageHeader";
-import { SkillsCard } from "./SkillsCard";
-
+import { techStacks, firstHalf, secondHalf } from "../data/techStacks";
+import { SkillCard } from "./SkillCard";
 export const Skills = () => {
-  const skills = [
-    {
-      id: 1,
-      header: "Frontend Development",
-      icons: [
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/html-5.svg",
-            alt: "icon html",
-          },
-          name: "HTML",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/css-3.svg",
-            alt: "icon css",
-          },
-          name: "CSS",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/tailwind.svg",
-            alt: "icon tailwind",
-          },
-          name: "Tailwind CSS",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/javascript.svg",
-            alt: "icon javaScript",
-          },
-          name: "JavaScript",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/react.svg",
-            alt: "icon react",
-          },
-          name: "React",
-        },
-      ],
-    },
-    {
-      id: 2,
-      header: "Backend Development",
-      icons: [
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/node-js.svg",
-            alt: "icon Node.js",
-          },
-          name: "Node.js",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/express.svg",
-            alt: "icon express",
-          },
-          name: "Express",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/mongo.svg",
-            alt: "icon mongo db",
-          },
-          name: "Mongo DB",
-        },
-      ],
-    },
-     {
-      id: 3,
-      header: "Tools",
-      icons: [
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/figma.svg",
-            alt: "icon figma",
-          },
-          name: "Figma",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/git.svg",
-            alt: "icon git",
-          },
-          name: "Git",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/vs-code.svg",
-            alt: "icon vs cdoe",
-          },
-          name: "VS Code",
-        },
-        {
-          icon: {
-            url: "./src/assets/images/skill-icons/vite.svg",
-            alt: "icon vite",
-          },
-          name: "Vite",
-        },
-      ],
-    },
-  ];
-  
   return (
-    <div className="min-h-[calc(100vh-68px)] py-16 px-4 md:px-12 bg-white flex flex-col gap-14 items-center">
-      <PageHeader
-        header={"Tech Stack"}
-        text={
-          "Here are the technologies and tools I work with to bring ideas to life"
-        }
-      />
-      <div className="flex flex-col lg:flex-row gap-6 md:gap-3 justify-between w-full">
-        {skills.map((skill) => {
-          return <SkillsCard key={skill.id} header={skill.header} icons={skill.icons} />;
-        })}
+    <div className="py-16 px-4 md:px-12 bg-softWhite flex flex-col gap-14 items-center">
+      <PageHeader header={"Tech Stack"} />
+
+      {/* animation scroll one line */}
+      <div className="w-screen overflow-hidden hidden md:flex flex-col">
+        <ul className="flex w-fit animate-scroll">
+          {[...techStacks, ...techStacks].map((techStack) => (
+            <SkillCard key={techStack.id} icon={techStack.icon} />
+          ))}
+        </ul>
+      </div>
+
+      {/* animation scroll two line */}
+      <div className="w-screen overflow-hidden flex md:hidden flex-col">
+        <ul className="flex w-fit animate-scroll">
+          {[...firstHalf, ...firstHalf].map((techStack) => (
+            <SkillCard key={techStack.id} icon={techStack.icon} />
+          ))}
+        </ul>
+        <ul className="flex w-fit animate-scroll-black">
+          {[...secondHalf, ...secondHalf].map((techStack) => (
+            <SkillCard key={techStack.id} icon={techStack.icon} />
+          ))}
+        </ul>
       </div>
     </div>
   );
