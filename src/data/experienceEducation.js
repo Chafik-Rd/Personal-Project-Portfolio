652133bacbef806a7d2c288a94840587601425c6
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-04 23:59:50 +0700

    doc(data): add datas mockup

---
+++ src/data/experienceEducation.js
@@ -0,0 +1,45 @@
+export const experience = [
+  {
+    id: 2,
+    jobPosition: "Engineer and Service",
+    company: "Saha Union Engineering",
+    jobType: "Full-time",
+    date: "Oct. 2022 - Apr. 2025",
+    location: "Bangkok, Thailand",
+    detail: [
+      "Built full SCADA systems: HMI, RTU, data mapping, testing, and CAD design",
+      "Installed and tested solar monitors with real-time client data delivery",
+    ],
+  },
+  {
+    id: 1,
+    jobPosition: "IoT Engineer",
+    company: "Stream I.T. Consulting",
+    jobType: "Internship",
+    date: "Jun. 2021 - Jul. 2021",
+    location: "Bangkok, Thailand",
+    detail: [
+      "Developed a Raspberry Pi-based detection system using Python.",
+      "Built a low-current measurement system using ESP32.",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 2,
+    name: "Junior Software Developer Bootcamp",
+    institution: "Generation Tahiland",
+    date: "Jun. 2025 - Sep. 2025",
+    location: "Bangkok, Thailand",
+    detail: ["Specialized in electronics engineering with focus on digital systems, embedded programming, and circuit design."],
+  },
+  {
+    id: 1,
+    name: "B.Eng. in Electronics Engineering",
+    institution: "King Mongkut's Institute of Technology Ladkrabang",
+    date: "2018 - 2022",
+    location: "Bangkok, Thailand",
+    detail: ["Specialized in electronics engineering with focus on digital systems, embedded programming, and circuit design."],
+  },
+];
