b2a9b81433986c9aff63a0ec85513ffd8489695e
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-09-26 00:58:43 +0700

    feat(send message): created send message to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,14 +6,43 @@ import { Send, Linkedin, Github } from "lucide-react";
 import { ContactCard } from "./ContactCard";
 import { connectsMe, contacts, linkCV } from "../data/contacts";
 import { ContactIcon } from "./ContactIcon";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const icons = {
   github: Github,
   linkedin: Linkedin,
   send: Send,
 };
 export const Contact = () => {
-  const handleSubmit = () => {
-    console.log("");
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .send(
+        import.meta.env.VITE_YOUR_SERVICE_ID,
+        import.meta.env.VITE_YOUR_TEMPLATE_ID,
+        form,
+        import.meta.env.VITE_YOUR_PUBLIC_KEY
+      )
+      .then(
+        () => alert("✅ Message sent successfully!"),
+        (error) => alert("❌ Failed to send: " + error.text)
+      );
+
+    setForm({
+      name: "",
+      email: "",
+      message: "",
+    });
   };
   return (
     <div
@@ -37,26 +66,54 @@ export const Contact = () => {
             </CardHeader>
             <CardContent className="flex flex-col gap-8">
               <form onSubmit={handleSubmit} className="flex flex-col gap-8">
-                <div>
-                  <label>Name</label>
-                  <Input type="text" placeholder="Your name" />
-                </div>
-                <div>
-                  <label>Email</label>
-                  <Input type="email" placeholder="Your.email@example.com" />
-                </div>
-                <div>
-                  <label>Message</label>
+                <label>
+                  <p>Name</p>
+                  <Input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="Your name"
+                    name="name"
+                    value={form.name}
+                    required
+                  />
+                </label>
+
+                <label>
+                  <p>Email</p>
+                  <Input
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Your.email@example.com"
+                    name="email"
+                    value={form.email}
+                    required
+                    pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
+                  />
+                </label>
+
+                <label>
+                  <p>Message</p>
                   <textarea
+                    onChange={handleChange}
                     placeholder="Your message..."
                     className="block border-1 p-4 rounded-md w-full h-60"
+                    name="message"
+                    value={form.message}
+                    required
                   ></textarea>
-                </div>
+                </label>
+                <Button
+                  type="submit"
+                  size="md"
+                  className={`w-full ${
+                    Object.values(form).some((item) => item.trim() === "") &&
+                    "bg-mutedBlue-300/70 pointer-events-none"
+                  }`}
+                >
+                  <Send size={16} />
+                  Send Message
+                </Button>
               </form>
-              <Button type="submit" size="md" className="w-full">
-                <Send size={16} />
-                Send Message
-              </Button>
             </CardContent>
           </Card>
         </section>
@@ -103,13 +160,13 @@ export const Contact = () => {
               I'm eager to grow, learn, and take on new challenges.
             </p>
             <a href={linkCV} target="_blank" rel="noopener noreferrer">
-            <Button
-              size="md"
-              variant="outline"
-              className="text-mutedBlue-300 sm:text-lg w-fit"
-            >
-              Download Resume
-            </Button>
+              <Button
+                size="md"
+                variant="outline"
+                className="text-mutedBlue-300 sm:text-lg w-fit"
+              >
+                Download Resume
+              </Button>
             </a>
           </div>
         </section>
